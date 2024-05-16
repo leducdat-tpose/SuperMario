7dd1203852219c14679dcc7bec7d618592983a9f
leducdat-tpose <leducdat0907@gmail.com>
2024-05-16 12:23:17 +0700

    Add luckyBox or mysteryBox. Don't has special collision with Mario or Koopas yet

---
+++ SuperMario/LuckyBox.cpp
@@ -0,0 +1,16 @@
+#include "LuckyBox.h"
+
+void CLuckyBox::Render()
+{
+	CAnimations* animations = CAnimations::GetInstance();
+	animations->Get(ID_ANI_LUCKYBOX)->Render(x, y);
+	RenderBoundingBox();
+}
+
+void CLuckyBox::GetBoundingBox(float& l, float& t, float& r, float& b)
+{
+	l = x - LUCKYBOX_BBOX_WIDTH / 2;
+	t = y - LUCKYBOX_BBOX_HEIGHT / 2;
+	r = l + LUCKYBOX_BBOX_WIDTH;
+	b = t + LUCKYBOX_BBOX_HEIGHT;
+}
---
+++ SuperMario/LuckyBox.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "GameObject.h"
+#include "Animation.h"
+#include "Animations.h"
+
+#define ID_ANI_LUCKYBOX 11000
+#define LUCKYBOX_WIDTH 16
+#define LUCKYBOX_BBOX_WIDTH 16
+#define LUCKYBOX_BBOX_HEIGHT 16
+
+class CLuckyBox : public CGameObject {
+public:
+	CLuckyBox(float x, float y) : CGameObject(x, y) {}
+	void Render();
+	void Update(DWORD dt) {}
+	void GetBoundingBox(float& l, float& t, float& r, float& b);
+};
