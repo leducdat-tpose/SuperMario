162e141f1f4927b53c7ee2ac708ba7646db0d02c
leducdat-tpose <leducdat0907@gmail.com>
2024-05-23 00:40:01 +0700

    Add Player.h, it contain value like point, what gadget is available

---
+++ SuperMario/Player.cpp
@@ -0,0 +1,10 @@
+#include "Player.h"
+#include "Define.h"
+
+CPlayer::CPlayer(LPGAME game, LPSCENE scene)
+{
+	this->game = game;
+	this->scene = scene;
+}
+
+CPlayer::~CPlayer(){}
---
+++ SuperMario/Player.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <vector>
+#include "PlayScene.h"
+
+using namespace std;
+
+class CPlayer
+{
+	LPGAME game;
+	LPSCENE scene;
+	//information of player such as point, gadget own or archive
+	string information;
+	int score;
+	int time;
+	vector<LPGAMEOBJECT> gadget;
+public:
+	CPlayer(LPGAME game, LPSCENE scene);
+	~CPlayer();
+
+	void Update(DWORD dt);
+};
