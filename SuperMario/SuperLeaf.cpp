3f932c576cae9baf5ebf665960d16d93bb3d1457
leducdat-tpose <leducdat0907@gmail.com>
2024-06-07 16:58:36 +0700

    Now, SuperLeaf can fall

--- SuperMario/SuperLeaf.cpp
+++ SuperMario/SuperLeaf.cpp
@@ -3,7 +3,9 @@
 CSuperLeaf::CSuperLeaf(float x, float y) :CGameObject(x, y)
 {
 	this->ax = 0;
-	this->ay = 0;
+	this->ay = SUPERLEAF_GRAVITY;
+	fall_start = -1;
+	SetState(SUPERLEAF_STATE_FALL_LEFT);
 }
 
 void CSuperLeaf::GetBoundingBox(float& left, float& top, float& right, float& bottom)
@@ -24,12 +26,25 @@ void CSuperLeaf::OnCollisionWith(LPCOLLISIONEVENT e)
 {
 	if (!e->obj->IsBlocking()) return;
 	if (dynamic_cast<CSuperLeaf*>(e->obj)) return;
-}
+	if (e->ny != 0)
+	{
+		vy = 0;
+		isCollidable = true;
+	}
+};
 
 void CSuperLeaf::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 {
-	vy += ay * dt;
+	vy = ay * dt;
 	vx += ax * dt;
+	if ((GetTickCount64() - fall_start > SUPERLEAF_TIME_TURN_STATE) && (state == SUPERLEAF_STATE_FALL_LEFT))
+	{
+		SetState(SUPERLEAF_STATE_FALL_RIGHT);
+	}
+	else if ((GetTickCount64() - fall_start > SUPERLEAF_TIME_TURN_STATE) && (state == SUPERLEAF_STATE_FALL_RIGHT))
+	{
+		SetState(SUPERLEAF_STATE_FALL_LEFT);
+	}
 	CGameObject::Update(dt, coObjects);
 	CCollision::GetInstance()->Process(this, dt, coObjects);
 }
@@ -39,4 +54,24 @@ void CSuperLeaf::Render()
 	int aniId = ID_ANI_SUPERLEAF;
 	CAnimations::GetInstance()->Get(aniId)->Render(x, y);
 	RenderBoundingBox();
+}
+
+void CSuperLeaf:: SetState(int state)
+{
+	CGameObject::SetState(state);
+	switch (state)
+	{
+	case SUPERLEAF_STATE_FALL_LEFT:
+	{
+		fall_start = GetTickCount64();
+		vx = -SUPERLEAF_SPEED;
+		break;
+	}
+	case SUPERLEAF_STATE_FALL_RIGHT:
+	{
+		fall_start = GetTickCount64();
+		vx = SUPERLEAF_SPEED;
+		break;
+	}
+	}
 }
--- SuperMario/SuperLeaf.h
+++ SuperMario/SuperLeaf.h
@@ -1,23 +1,34 @@
 #pragma once
 #include "GameObject.h"
 
+#define SUPERLEAF_GRAVITY 0.001f
+#define SUPERLEAF_SPEED 0.04f
+#define SUPERLEAF_TIME_TURN_STATE 500
+
 #define SUPERLEAF_BBOX_WIDTH 16
 #define SUPERLEAF_BBOX_HEIGHT 16
 
 #define ID_ANI_SUPERLEAF 18000
+
+#define SUPERLEAF_STATE_FALL_LEFT 100
+#define SUPERLEAF_STATE_FALL_RIGHT 200
+
 
 class CSuperLeaf : public CGameObject
 {
 protected:
 	float ax;
 	float ay;
+	ULONGLONG fall_start;
+	bool isCollidable = false;
 	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
 	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
 	virtual void Render();
-	virtual int IsCollidable() { return 1; };
-	virtual int IsBlocking() { return 0; }
+	virtual int IsCollidable() { return !isCollidable; };
+	virtual int IsBlocking() { return 0; };
 	virtual void OnNoCollision(DWORD dt);
 	virtual void OnCollisionWith(LPCOLLISIONEVENT e);
 public:
 	CSuperLeaf(float x, float y);
+	void SetState(int state);
 };
